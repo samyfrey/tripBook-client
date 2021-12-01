e007f45563fe0a172953c748943d2f02da533de6
samyfrey <samyfrey@yahoo.com>
2021-12-01 00:28:20 -0500

    done with authentication and create resource

--- app/app.js
+++ app/app.js
@@ -1,9 +1,11 @@
-// use require with a reference to bundle the file and use it in this file
-// const example = require('./example')
-
-// use require without a reference to ensure a file is bundled
-// require('./example')
-
+const authEvents = require('./auth/events')
+const uiNav = require('./auth/ui')
+const tripEvents = require('./trips/events')
 $(() => {
-  // your JS code goes here
+  $('#sign-up').on('submit', authEvents.onSignUp)
+  $('#sign-in').on('submit', authEvents.onSignIn)
+  $('#change-password').on('submit', authEvents.onChangePassword)
+  $('#sign-out').on('click', authEvents.onSignOut)
+  // $('#post-form').on('submit', authEvents.onPostSubmit)
+  $('#post-create').on('submit', tripEvents.onCreatePost)
 })
